6dac22488faa532b88b6d6fe0a6904a0d3c74166
JenIKs <fertuv68@gmail.com>
2022-05-23 00:04:33 +0300

    click

--- glav.py
+++ glav.py
@@ -1,5 +1,22 @@
-print('hello')
-print('hi')
-print('mym')
-print("qq")
-print('obnova')
+from tkinter import *
+from tkinter import ttk
+
+def plus():
+    global count
+    count += 1
+    coun['text'] = count
+
+
+root = Tk()
+
+root.title('First install')
+root.geometry('100x50')
+
+count = 0
+
+coun = Label(root, text=count)
+coun.pack()
+
+button = ttk.Button(root, text='Click', command=plus)
+button.pack()
+root.mainloop()
