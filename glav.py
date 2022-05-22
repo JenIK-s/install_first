3f718f7ad09c80d23066de89f9212d8de34c5b89
SenselessName <evseenkomikhail@mail.ru>
2022-05-23 00:07:55 +0300

    ClickUpdate

--- glav.py
+++ glav.py
@@ -6,11 +6,16 @@ def plus():
     count += 1
     coun['text'] = count
 
+def minus():
+    global count
+    count -= 1
+    coun['text'] = count
+
 
 root = Tk()
 
 root.title('First install')
-root.geometry('100x50')
+root.geometry('100x100')
 
 count = 0
 
@@ -19,4 +24,6 @@ coun.pack()
 
 button = ttk.Button(root, text='Click', command=plus)
 button.pack()
+button2 = ttk.Button(root, text='unclick', command=minus)
+button2.pack()
 root.mainloop()
