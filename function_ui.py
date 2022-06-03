956971a7a8e05747c7eec337e97db2e3445221d3
JenIK-s <96344210+JenIK-s@users.noreply.github.com>
2022-06-04 00:45:44 +0300

    beta_0_1

--- function_ui.py
+++ function_ui.py
@@ -6,37 +6,37 @@ from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTi
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
 import time
-
-def yes():
-    quit()
+from glav import *
 
 
+class UIFunctions(MainWindow):
+    def yes():
+        quit()
 
 
-def toggleMenu(self, maxWidth, enable):
-    if enable:
-        # GET WIDTH
-        width = self.ui.frame_left_menu.width()
-        maxExtend = maxWidth
-        standard = 70
-        # SET MAX WIDTH
-        if width == 70:
-            widthExtended = maxExtend
-        else:
-            widthExtended = standard
-        # ANIMATION
-        self.ui.btn_page_1.setText('Браузеры')
-        self.ui.btn_page_2.setText('Редакторы')
-        self.ui.btn_page_3.setText('Утилиты')
 
-        self.animation = QPropertyAnimation(self.ui.frame_left_menu, b"minimumWidth")
-        self.animation.setDuration(800)
-        self.animation.setStartValue(width)
-        self.animation.setEndValue(widthExtended)
-        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
-        self.animation.start()
-        if width == 250:
-            self.ui.btn_page_1.setText('Б...')
-            self.ui.btn_page_2.setText('Р...')
-            self.ui.btn_page_3.setText('У...')
+
+    def toggleMenu(self, maxWidth, enable):
+        if enable:
+            # GET WIDTH
+            width = self.ui.frame_left_menu.width()
+            maxExtend = maxWidth
+            standard = 0
+            # SET MAX WIDTH
+            if width == 0:
+                widthExtended = maxExtend
+            else:
+                widthExtended = standard
+            # ANIMATION
+            self.ui.btn_page_1.setText('Браузеры')
+            self.ui.btn_page_2.setText('Редакторы')
+            self.ui.btn_page_3.setText('Утилиты')
+
+            self.animation = QPropertyAnimation(self.ui.frame_left_menu, b"minimumWidth")
+            self.animation.setDuration(800)
+            self.animation.setStartValue(width)
+            self.animation.setEndValue(widthExtended)
+            self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animation.start()
+
 
--- glav.py
+++ glav.py
@@ -1,7 +1,7 @@
 import sys
 import platform
 
-import function_ui
+#import function_ui
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
@@ -26,20 +26,75 @@ class MainWindow(QMainWindow):
         self.show()
 
 
-        self.ui.pushButton.clicked.connect(lambda: function_ui.yes())
-        self.ui.Btn_Toggle.clicked.connect(lambda: function_ui.toggleMenu(self, 250, True))
+        self.ui.pushButton.clicked.connect(lambda: UIFunctions.yes())
+        self.ui.Btn_Toggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 250, True))
         self.ui.btn_page_1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_1))
         self.ui.btn_page_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_2))
         self.ui.btn_page_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
         self.ui.pushButton_3.clicked.connect(lambda: self.showMinimized())
 
-        self.ui.btn_page_1.setText('Б...')
-        self.ui.btn_page_2.setText('Р...')
-        self.ui.btn_page_3.setText('У...')
+        self.ui.btn_page_1.clicked.connect(lambda: self.ui.label_5.setText('Браузеры'))
+        self.ui.btn_page_2.clicked.connect(lambda: self.ui.label_5.setText('Редакторы'))
+        self.ui.btn_page_3.clicked.connect(lambda: self.ui.label_5.setText('Утилиты'))
 
+
+        self.ui.frame_left_menu.setMinimumSize(QtCore.QSize(0, 0))
+        self.ui.frame_left_menu.setMaximumSize(QtCore.QSize(0, 16777215))
+
+        self.ui.pushButton.setGeometry(QtCore.QRect(910, 0, 41, 41))
+        self.ui.pushButton_3.setGeometry(QtCore.QRect(870, 0, 41, 41))
+
+
+        #-------------------------ИКОНКИ-------------------------
+        #self.ui.btn_page_1.setIcon(QtGui.QIcon('chrome_browser_logo_icon_153007.png'))
+        self.ui.Btn_Toggle.setIcon(QtGui.QIcon('hamburger_button_menu_icon_155296.png'))
+        self.ui.label_2.setPixmap(QtGui.QPixmap('chrome_browser_logo_icon_153007 (1).png'))
+        #--------------------------------------------------------
         self.ui.btn_page_1.setFont(QFont('Segoe UI', 14))
         self.ui.btn_page_2.setFont(QFont('Segoe UI', 14))
         self.ui.btn_page_3.setFont(QFont('Segoe UI', 14))
+        self.ui.Btn_Toggle.setFont(QFont('Segoe UI', 14))
+        self.ui.pushButton_2.setFont(QFont('Segoe UI',16 , QtGui.QFont.Bold))
+        self.ui.pushButton.setFont(QFont('Segoe UI', 12))
+        self.ui.pushButton_3.setFont(QFont('Segoe UI', 14))
+
+        self.ui.label_5.setFont(QFont('Segoe UI', 13))
+
+
+
+        self.ui.label_4.setFont(QFont('Segoe UI', 20, QtGui.QFont.Bold))
+
+    def mousePressEvent(self, event):
+        # Если нажата левая кнопка мыши
+        if event.button() == QtCore.Qt.LeftButton:
+            # получаем координаты окна относительно экрана
+            x_main = self.geometry().x()
+            y_main = self.geometry().y()
+            # получаем координаты курсора относительно окна нашей программы
+            cursor_x = QtGui.QCursor.pos().x()
+            cursor_y = QtGui.QCursor.pos().y()
+            # проверяем условием позицию курсора на нужной области программы(у нас это верхний бар)
+            # если всё ок - перемещаем
+            # иначе игнорируем
+            if x_main <= cursor_x <= x_main + self.geometry().width():
+                if y_main <= cursor_y <= y_main + self.ui.frame_top.geometry().height():
+                    self.old_pos = event.pos()
+                else:
+                    self.old_pos = None
+        elif event.button() == QtCore.Qt.RightButton:
+            self.old_pos = None
+
+    # вызывается при отпускании кнопки мыши
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.old_pos = None
+
+    # вызывается всякий раз, когда мышь перемещается
+    def mouseMoveEvent(self, event):
+        if not self.old_pos:
+            return
+        delta = event.pos() - self.old_pos
+        self.move(self.pos() + delta)
 
 
 
